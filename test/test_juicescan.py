ae1591dc801d6772ce43bc1458f04bfbe87c3c57
Paul-Antoine Armani <parmani@W11P-ARMANI.localdomain>
2022-11-04 16:28:36 +0100

    CI/CL fix

--- test/test_juicescan.py
+++ test/test_juicescan.py
@@ -1,2 +1,9 @@
-def test() -> None:
-    assert 1 == 1
+from juicescan.parser import is_valid_ip_address
+
+
+def test_is_valid_ip_address_ok() -> None:
+    assert is_valid_ip_address("192.168.0.1")
+
+
+def test_is_valid_ip_address_ko() -> None:
+    assert not is_valid_ip_address("abc123.4.5.6.7")
